0122db411e812830852db4c1656f4e178e38d26a
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-13 16:34:37 +0100

    feat: button bonus

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-for(i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(element => {
-                console.log(element.data);
-                document.getElementById('output').innerHTML += `<li>${element.data.response}</li>`;
-                
-            })
-            .catch(error => {
-                console.error('Errore nella richiesta:', error);
-                document.getElementById('output').textContent = 'Errore nel caricamento dei dati!';
-            });
+const output = document.getElementById('output');
+const button = document.getElementById('button');
+const getEmails = function() {
+    for(i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(element => {
+                    console.log(element.data);
+                    output.innerHTML += `<li>${element.data.response}</li>`;
+                    
+                })
+                .catch(error => {
+                    console.error('Errore nella richiesta:', error);
+                    output.innerHTML = `<li>errore nel caricamento dei dati</li>`;
+                });
+    };
 };
+getEmails();
+button.addEventListener('click', function(){
+    output.innerHTML = ``;
+    getEmails();
+});
